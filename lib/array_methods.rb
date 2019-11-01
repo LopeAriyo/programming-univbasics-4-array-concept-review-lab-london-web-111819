985ab7a5c1ea6e3f4badd72ab1288405af40282c
Lopè Ariyo <lopeariyo@gmail.com>
2019-11-01 12:14:48 +0000

    Automatically backed up by Learn

--- lib/array_methods.rb
+++ lib/array_methods.rb
@@ -5,9 +5,10 @@ def find_element_index(array, value_to_find)
  while array[counter] do
 
   if array[counter] == value_to_find
-    p counter
+    puts counter
   end
 
+  p counter
   counter += 1
 
  end
